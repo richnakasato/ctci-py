b3878692f1dfd47c529304db69343f948b026535
Rich Nakasato <richnakasato@hotmail.com>
2018-08-30 23:25:21 -0400

    coded up string compress

--- string_compress.py
+++ string_compress.py
@@ -1,9 +1,31 @@
 def string_compression(string):
-    pass
+    # TODO: error handling
+    new_string = list()
+    curr_char = string[0]
+    count = 1
+    idx = 1
+    while idx < len(string):
+        if curr_char == string[idx]:
+            count += 1
+        else:
+            new_string.append(curr_char)
+            new_string.append(str(count))
+            curr_char = string[idx]
+            count = 1
+        idx += 1
+    new_string.append(curr_char)
+    new_string.append(str(count))
+    out_string = ''.join(new_string)
+    if len(string) <= len(out_string):
+        return string
+    return out_string
 
 
 def main():
-    pass
+    string = 'aabcccccaaa'
+    print(string_compression(string))
+    string2 = 'a'
+    print(string_compression(string2))
 
 if __name__ == "__main__":
     main()
