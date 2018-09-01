2459fed21be0cb9df30aded7e1b9c0698ed5a6ee
Rich Nakasato <richnakasato@hotmail.com>
2018-08-31 22:57:49 -0400

    implemented O(n^2) space version (where n is size of row in square matrix), need to work on inplace

--- rotate_matrix.py
+++ rotate_matrix.py
@@ -1,10 +1,24 @@
 def rotate_matrix(matrix):
-    pass
+    n = len(matrix)
+    out_matrix = [[None for x in range(n)] for y in range(n)]
+    for x in range(n):
+        for y in range(n):
+            out_matrix[y][n-x-1] = matrix[x][y]
+    return out_matrix
+
+def rotate_matrix_inplace(matrix):
+    raise Exception('not implemented!')
 
 
 def main():
-    pass
+    matrix = [[None for x in range(3)] for y in range (3)]
+    for x in range(3):
+        for y in range(3):
+            matrix[x][y] = (x, y)
+    print(matrix)
+    print(rotate_matrix(matrix))
+    print(rotate_matrix_inplace(matrix))
 
 if __name__ == "__main__":
-    main():
+    main()
 
