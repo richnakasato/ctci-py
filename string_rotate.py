2e67703b0ae34ca928375902a18cf49da176989e
Rich Nakasato <richnakasato@hotmail.com>
2018-09-01 15:07:05 -0400

    added brute force version of string rotate that does not use isSubstring helper

--- string_rotate.py
+++ string_rotate.py
@@ -1,9 +1,31 @@
-def string_rotate(str_a, str_b):
+# O(n) time and space
+def rotate_left(string, n):
+    str_len = len(string)
+    offset = str_len - n
+    temp = [None] * str_len
+    for i in range(str_len):
+        temp[i] = string[(i-offset)%str_len]
+    return ''.join(temp)
+
+# O(n^2) time, O(n) space?
+def string_rotate_bf(original, rotated):
+    # TODO: check len, error handle
+    max_rotations = len(original)
+    for i in range(max_rotations):
+        guess = rotate_left(original, i)
+        if guess == rotated:
+            return True
+    return False
+
+def string_rotate_substring(original, rotated):
     pass
 
 
 def main():
+    string = 'racetrack'
+    n = len(string) - 1
+    print(rotate_left(string, n))
     pass
 
 if __name__ == "__main__":
-    pass
+    main()
