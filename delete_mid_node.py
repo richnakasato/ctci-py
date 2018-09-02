d076e1f834777e0c40b4fc26ab664303bbfb259b
Rich Nakasato <richnakasato@hotmail.com>
2018-09-01 22:40:40 -0400

    initial commit

---
+++ delete_mid_node.py
@@ -0,0 +1,31 @@
+class SinglyLinkedList():
+
+    class Node():
+
+        def __init__(self, data, next_=None):
+            self.data = data
+            self.next = next_
+
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def append(self, data):
+        pass
+
+    def delete(self, data, all_=False):
+        pass
+
+    def delete_middle_node_bf(self):
+        pass
+
+    def delete_middle_node_op(self):
+        pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
