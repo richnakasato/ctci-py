3dd62fdd49d177cf24726c97030e9079b7b00709
Rich Nakasato <richnakasato@hotmail.com>
2018-09-03 15:57:04 -0400

    initial commit

---
+++ partition.py
@@ -0,0 +1,34 @@
+import random
+
+class SinglyLinkedList():
+
+    class Node():
+
+        def __init__(self, data, next_=None):
+            pass
+
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def append(self, data):
+        pass
+
+    def delete(self, data, all_=False):
+        pass
+
+    def partition(self, partition):
+        pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
