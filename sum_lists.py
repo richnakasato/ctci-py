d3d672b57b5878b6c7717154026095dc99d59077
Rich Nakasato <richnakasato@hotmail.com>
2018-09-06 23:49:01 -0400

    working version of reverse sum with sentinel node

--- sum_lists.py
+++ sum_lists.py
@@ -24,18 +24,81 @@ class SinglyLinkedList():
         curr = self.head
         while curr:
             outstring += str(curr)
+            curr = curr.next
         outstring += 'NULL'
         return outstring
 
     def append(self, data):
-        pass
+        self.size += 1
+        if not self.head:
+            self.head = self.Node(data)
+        else:
+            curr = self.head
+            while curr.next:
+                curr = curr.next
+            curr.next = self.Node(data)
 
     def delete(self, data):
-        pass
+        if not self.head:
+            raise Exception('empty list!')
+        elif self.head == data:
+            size -= 1
+            self.head = self.head.next
+        else:
+            curr = self.head
+            while curr.next:
+                if curr.next.data == data:
+                    self.size -= 1
+                    curr.next = curr.next.next
+                    return
+                curr = curr.next
 
+class LLNode():
+
+    def __init__(self, data, next_=None):
+        self.data = data
+        self.next = next_
+
+
+def sum_lists(left_list, right_list):
+    # TODO: error handling
+    left = left_list.head
+    right = right_list.head
+    curr = dummy = LLNode(-999)
+    carry = 0
+    while left and right:
+        sum_ = left.data + right.data + carry
+        carry = sum_//10
+        curr.next = LLNode(sum_%10)
+        curr, left, right = curr.next, left.next, right.next
+    while left:
+        curr.next = LLNode(left.data)
+        curr, left = curr.next, left.next
+    while right:
+        curr.next = LLNode(right.data)
+        curr, right = curr.next, right.next
+    return dummy.next
 
 def main():
-    pass
+    left = SinglyLinkedList()
+    left.append(7)
+    left.append(1)
+    left.append(6)
+    right = SinglyLinkedList()
+    right.append(5)
+    right.append(9)
+    right.append(2)
+    print(left)
+    print(right)
+    sum_ = sum_lists(left, right)
+    curr = sum_
+    outstring = ''
+    while curr:
+        outstring += '{}-> '.format(curr.data)
+        curr = curr.next
+    outstring += 'NULL'
+    print(outstring)
+
 
 if __name__ == '__main__':
     main()
