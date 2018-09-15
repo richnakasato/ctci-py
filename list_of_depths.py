c3b11a57de636167bbbb246d94fb9d2f03105311
Rich Nakasato <richnakasato@hotmail.com>
2018-09-15 14:58:11 -0400

    initial commit

---
+++ list_of_depths.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+class TreeNode:
+
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
