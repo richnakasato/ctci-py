098623d4ef0d7874eea9c8e2b4c6ea51932c8fcf
Rich Nakasato <richnakasato@hotmail.com>
2018-10-14 00:27:08 -0400

    dfs iterative validation

--- validate_bst.py
+++ validate_bst.py
@@ -5,9 +5,54 @@ class Node():
         self.left = None
         self.right = None
 
+def validate_bst(root):
+    stack = list()
+    min_ = float('-inf')
+    max_ = float('inf')
+    stack.append((min_, root, max_))
+    while stack:
+        min_, curr, max_ = stack.pop()
+        if not min_ < curr.data < max_:
+            return False
+        if curr.left:
+            stack.append((min_, curr.left, curr.data))
+        if curr.right:
+            stack.append((curr.data, curr.right, max_))
+    return True
+
 
 def main():
-    pass
+    node1 = Node(1)
+    node2 = Node(2)
+    node3 = Node(3)
+    node4 = Node(4)
+    node5 = Node(5)
+    node6 = Node(6)
+    node7 = Node(7)
+    root = node4
+    node4.left = node2
+    node4.right = node6
+    node2.left = node1
+    node2.right = node3
+    node6.left = node5
+    node6.right = node7
+    assert validate_bst(root), 'bst is invalid!'
+
+    node1 = Node(1)
+    node2 = Node(2)
+    node3 = Node(3)
+    node4 = Node(4)
+    node5 = Node(6)
+    node6 = Node(5)
+    node7 = Node(7)
+    root = node4
+    node4.left = node2
+    node4.right = node6
+    node2.left = node1
+    node2.right = node3
+    node6.left = node5
+    node6.right = node7
+    assert not validate_bst(root), 'bst is valid!'
 
 if __name__ == "__main__":
     main()
