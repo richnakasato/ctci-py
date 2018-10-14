188e0d6b0ffba2f66ae81ff8ac1efa43073c9a86
Rich Nakasato <richnakasato@hotmail.com>
2018-10-13 22:12:38 -0400

    initial commit

---
+++ validate_bst.py
@@ -0,0 +1,14 @@
+class Node():
+
+    def __init__(self, data=None):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
